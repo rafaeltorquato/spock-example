79ca53f6b356e3fcf75adfa1ac2baa24c2878b1f
Rafael Torquato <rafael.torquato@ebserh.gov.br>
2021-06-21 19:59:47 -0300

    ♻️ Refactoring

--- src/main/java/com/torquato/spock/SpockExampleApplication.java
+++ src/main/java/com/torquato/spock/SpockExampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BddExampleApplication {
+public class SpockExampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BddExampleApplication.class, args);
+        SpringApplication.run(SpockExampleApplication.class, args);
     }
 
 }
